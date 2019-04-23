fb303d27fd0b8da846927c19505f9e1555e143c5
Andrew Barber <dewiepedia@gmail.com>
2019-04-23 19:33:37 -0300

    Split things out in to two versions. Including Arduino files for NodeMCU

---
+++ V1.0/Arduino/ReturnDepthAsJSON/ReturnDepthAsJSON.ino
@@ -0,0 +1,80 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+ 
+const char* ssid = "255 Stevens Rd";
+const char* password = "dewie123";
+
+int ledPin = 13; // GPIO13
+WiFiServer server(80);
+
+
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Turn on the LED (used to flash to indicate we sent data)
+  digitalWrite(ledPin, HIGH);
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+    // Prepare the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: application/json");
+  client.println(""); //  do not forget this one
+   
+  client.print("{\"IDkey\":\"WellA\",\"sensors\":{\"1\":{\"type\":\"water_depth\",\"value\":");
+  client.print(analogRead(A0));
+  client.println("}}}\r\n\r\n");
+  digitalWrite(ledPin, LOW);
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+
+  digitalWrite(ledPin, LOW);
+ 
+}
+ 
